4b96bc5ec1ba905854cba32713aa761a07536673
Matthew Tan <mtwtan@gmail.com>
2020-07-23 12:03:20 -0400

    Daily Deaths

--- nova/urls.py
+++ nova/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('plt_nova', views.plt_nova_view, name='plt_nova_view'),
     path('plt_nova_casechange', views.plt_nova_casechange_view, name='plt_nova_casechange_view'),
     path('plt_nova_mavg', views.plt_nova_movingavg_view, name='plt_nova_movingavg'),
-    path('plt_nova_casechange_view', views.plt_nova_casechange_view, name='plt_nova_casechange_view'),
+    path('plt_nova_changedeath_view', views.plt_nova_changedeath_view, name='plt_nova_changedeath_view'),
     path('', views.index, name='index'),
 ]
