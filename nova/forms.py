9d252b53ba78c90f6049dad6ec5f073e88b8cb2b
Matthew Tan <mtwtan@gmail.com>
2020-07-23 22:39:53 -0400

    filter

--- nova/forms.py
+++ nova/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from .models import Counties
 from django.forms import ModelChoiceField
 class FilterByCountyForm(forms.Form):
-  county = forms.ModelChoiceField(queryset=Counties.objects.using('data').all(),values_list('county'))
+  county = forms.ModelChoiceField(queryset=Counties.objects.using('data').values_list('county'))
