74d0cb9310d47ef29268aa4aa37e97b9e0791f4a
Matthew Tan <mtwtan@gmail.com>
2020-07-23 23:53:35 -0400

    filter

--- nova/forms.py
+++ nova/forms.py
@@ -7,8 +7,13 @@ COUNTY =(
     ("District of Columbia", "District of Columbia"), 
     ("Fairfax", "Fairfax"), 
     ("Montgomery", "Montgomery"),
-) 
+)
 
+def get_counties():
+  counties = ()
+  for county in Counties.objects.using('data').values_list('county'):
+    counties = counties + ((county.data, county.data),)
+  return counties
 class FilterByCountyForm(forms.Form):
   #county = forms.ModelChoiceField(queryset=Counties.objects.using('data').values_list('county'))
-  county = forms.ChoiceField(choices = COUNTY)  
+  county = forms.ChoiceField(choices = get_counties())  
