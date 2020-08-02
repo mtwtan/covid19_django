26e3698d1edc2ed6eaeec7aa8f57b1a79e9d1287
mtwtan <mtwtan@gmail.com>
2020-08-02 16:42:52 -0400

    Update settings.py

--- covid19/settings.py
+++ covid19/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@s=0+b)476a$rt!t$7tqk$$hy3z%762!yhs&8!l$uaa8_z(a8k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
